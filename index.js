c29437fa2c752444bc9182f3708372085058492a
Jonathan Canola <jonathancanola7@gmail.com>
2020-09-18 19:45:46 -0700

    not final

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
 
+let votingAge;
 
+console.log(true)
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
